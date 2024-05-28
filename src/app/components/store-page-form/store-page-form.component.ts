bd8ed634405c637326480b2af879f514e331a45b
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-05-27 21:51:52 -0600

    Add: Se crea [store-page-form.component].

---
+++ src/app/components/store-page-form/store-page-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'store-page-form',
+  templateUrl: './store-page-form.component.html',
+  styleUrls: ['./store-page-form.component.css'],
+  standalone: true,
+})
+export class StorePageFormComponent {}
