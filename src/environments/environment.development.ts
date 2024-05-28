56ecad18dd5d697425cc93bb524886f013dfa2a8
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-05-27 18:51:53 -0600

    Update: Se agregan variables [environments].

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   API_URL: 'https://fakestoreapi.com/',
+  APP_TITLE: 'Prueba Técnica [DEV]'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    API_URL: 'https://fakestoreapi.com/',
+    APP_TITLE: 'Prueba Técnica [DEV]'
+};
