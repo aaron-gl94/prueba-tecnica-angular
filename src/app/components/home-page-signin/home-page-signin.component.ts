3cdb49f5a1ad7a7f94901cdb72d39d8fa0ca5f57
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-05-29 21:51:06 -0600

    Delete: Reestructura de carpetas [components].

--- src/app/components/home-page-signin/home-page-signin.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'home-page-signin',
-  templateUrl: './home-page-signin.component.html',
-  styleUrls: ['./home-page-signin.component.css'],
-  standalone: true,
-})
-export class HomePageSigninComponent {}
--- src/app/components/store-page-form/store-page-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'store-page-form',
-  templateUrl: './store-page-form.component.html',
-  styleUrls: ['./store-page-form.component.css'],
-  standalone: true,
-})
-export class StorePageFormComponent {}
