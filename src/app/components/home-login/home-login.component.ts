f41a5d09533060936283644414e599adeca403b2
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-05-29 22:37:07 -0600

    Add: Se crea componente HomeLogin [HomePage/HomeLogin].

---
+++ src/app/components/home-login/home-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-login',
+  templateUrl: './home-login.component.html',
+  styleUrls: ['./home-login.component.css'],
+  standalone: true
+})
+export class HomeLoginComponent {
+
+}
