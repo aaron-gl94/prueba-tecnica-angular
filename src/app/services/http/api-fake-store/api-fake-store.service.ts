29fbe67c1e0afcfb59a78b004d5a56f8e809322d
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-05-29 22:05:48 -0600

    Update: Se actualizan los metodos de API con elementos RxJS [ApiFakeStore].

--- src/app/services/http/api-fake-store/api-fake-store.service.ts
+++ src/app/services/http/api-fake-store/api-fake-store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { ProductInterface } from 'src/app/interfaces/product.interface';
 
@@ -12,34 +12,86 @@ export class ApiFakeStoreService {
 
   // Método para obtener todos los productos
   getAllProducts(): Observable<ProductInterface[]> {
-    return this.httpClient.get<ProductInterface[]>(
-      `${environment.API_URL}/products`
+    return this.httpClient.get<ProductInterface[]>(`${environment.API_URL}/products`)
+    .pipe(
+      map(response => {
+        console.log(`[apiFakeStoreService.getAllProducts] | Response:`, response);
+        return response;
+      }),
+      catchError( error => {
+        let message = `[apiFakeStoreService.getAllProducts] | Error: ${error.message}`;
+
+        console.error(message);
+        throw new Error(message);
+      })
     );
   }
 
   // Método para obtener un producto por ID
-  getProductById(productId: number): Observable<ProductInterface> {
-    return this.httpClient.get<ProductInterface>(
-      `${environment.API_URL}/products/${productId}`
+  public getProductById(idProduct: number): Observable<ProductInterface> {
+    return this.httpClient.get<ProductInterface>(`${environment.API_URL}/products/${idProduct}`)
+    .pipe(
+      map(response => {
+        console.log(`[apiFakeStoreService.getProductById(${idProduct})] | Response:`, response);
+        return response;
+      }),
+      catchError( error => {
+        let message = `[apiFakeStoreService.getProductById(${idProduct})] | Error: ${error.message}`;
+
+        console.error(message);
+        throw new Error(message);
+      })
     );
   }
 
   // Método para obtener productos ordenados ascendente o descendente
   getProductsSorted(order: 'asc' | 'desc'): Observable<ProductInterface[]> {
-    return this.httpClient.get<ProductInterface[]>(
-      `${environment.API_URL}/products?sort=${order}`
-    );
+    return this.httpClient.get<ProductInterface[]>(`${environment.API_URL}/products?sort=${order}`)
+    .pipe(
+      map(response => {
+        console.log(`[apiFakeStoreService.getProductsSorted] | Response:`, response);
+        return response;
+      }),
+      catchError( error => {
+        let message = `[apiFakeStoreService.getProductsSorted] | Error: ${error.message}`;
+
+        console.error(message);
+        throw new Error(message);
+      })
+    ); 
   }
 
   // Método para obtener todas las categorías de productos
   getAllCategories(): Observable<string[]> {
-    return this.httpClient.get<string[]>(`${environment.API_URL}/products/categories`);
+    return this.httpClient.get<string[]>(`${environment.API_URL}/products/categories`)
+    .pipe(
+      map(response => {
+        console.log(`[apiFakeStoreService.getAllCategories] | Response:`, response);
+        return response;
+      }),
+      catchError( error => {
+        let message = `[apiFakeStoreService.getAllCategories] | Error: ${error.message}`;
+
+        console.error(message);
+        throw new Error(message);
+      })
+    );
   }
 
   // Método para obtener productos por categoría
   getProductsByCategory(category: string): Observable<ProductInterface[]> {
-    return this.httpClient.get<ProductInterface[]>(
-      `${environment.API_URL}/products/category/${category}`
+    return this.httpClient.get<ProductInterface[]>(`${environment.API_URL}/products/category/${encodeURIComponent(category)}`)
+    .pipe(
+      map(response => {
+        console.log(`[apiFakeStoreService.getProductsByCategory(${category})] | Response:`, response);
+        return response;
+      }),
+      catchError( error => {
+        let message = `[apiFakeStoreService.getProductsByCategory(${category})] | Error: ${error.message}`;
+
+        console.error(message);
+        throw new Error(message);
+      })
     );
   }
 }
